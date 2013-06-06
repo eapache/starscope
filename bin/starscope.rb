5f229765b67464e8925e63273a699275041eddcc
Evan Huus <evan.huus@jadedpixel.com>
2013-06-06 10:54:29 -0400

    Parse the ruby

--- bin/starscope.rb
+++ bin/starscope.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
 lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
--- lib/starscope/parsers/ruby.rb
+++ lib/starscope/parsers/ruby.rb
@@ -1,9 +1,13 @@
 require "starscope/parser"
+require "parser/current"
 
 module StarScope::Parsers
   class Ruby < StarScope::Parser
     def self.parse(file)
+      return unless file =~ /.*\.rb/
+      contents = IO.read(file)
       puts file
+      p Parser::CurrentRuby.parse(contents)
     end
   end
 end
