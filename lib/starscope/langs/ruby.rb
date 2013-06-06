d8457446ce37ea786cfd660ea07c8e974080e0ea
Evan Huus <evan.huus@jadedpixel.com>
2013-06-06 14:27:39 -0400

    More refactor, way better scope handling

--- lib/starscope/langs/ruby.rb
+++ lib/starscope/langs/ruby.rb
@@ -10,30 +10,62 @@ module StarScope::Lang
 
     def self.extract(file, &block)
       ast = Parser::CurrentRuby.parse_file(file)
-      extract_tree ast, file, &block
+      Extractor.new(ast, file).extract &block
     end
 
     private
 
-    def self.extract_tree(ast, file, &block)
-      extract_node ast, file, &block
-      ast.children.each {|node| extract_tree node, file, &block if node.is_a? AST::Node}
-    end
-
-    def self.extract_node(node, file, &block)
-      if node.type == :send
-        loc = StarScope::Location.new(file, node.source_map.expression.line)
-        yield :send, scoped_name(node), loc
+    class Extractor
+      def initialize(ast, file)
+        @ast = ast
+        @file = file
+        @scope = []
       end
-    end
+      
+      def extract(&block)
+        extract_tree(@ast, &block)
+      end
 
-    def self.scoped_name(node)
-      if node.children[0].is_a? AST::Node
-        scoped_name(node.children[0]) << node.children[1]
-      elsif node.children[0].is_a? Symbol
-        [node.children[0]]
-      else
-        [node.children[1]]
+      private
+
+      def extract_tree(tree, &block)
+        extract_node tree, &block
+
+        scope_count = 0
+        if [:class, :module].include? tree.type
+          new_scope = scoped_name(tree.children[0])
+          @scope << new_scope
+          scope_count = new_scope.count
+        end
+
+        tree.children.each {|node| extract_tree node, &block if node.is_a? AST::Node}
+
+        @scope.pop(scope_count)
+      end
+
+      def extract_node(node)
+        if node.type == :send
+          loc = StarScope::Location.new(@file, node.source_map.expression.line)
+          yield :send, scoped_name(node), loc
+        end
+      end
+
+      def scoped_name(node)
+        if node.is_a? Symbol
+          [node]
+        elsif node.is_a? AST::Node
+          if node.children[0].is_a? Symbol
+            [node.children[0]]
+          elsif node.children[0].is_a? AST::Node
+            scoped_name(node.children[0]) << node.children[1]
+          elsif node.children[0].nil?
+            if node.type == :const
+              [node.children[1]]
+            else
+              @scope + [node.children[1]]
+            end
+          end
+        end
       end
     end
   end
