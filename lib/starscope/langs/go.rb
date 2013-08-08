4131a80f4e0b05d1b7ce24119c9f1933cd899094
Evan Huus <evan.huus@jadedpixel.com>
2013-08-08 16:51:47 -0400

    Basic defs

--- lib/starscope/langs/go.rb
+++ lib/starscope/langs/go.rb
@@ -7,16 +7,16 @@ module StarScope::Lang
     def self.extract(file)
       str = File.readlines(file).each_with_index do |line, line_no|
         line.scan(/^func (\w+)/) do |name|
-          p "matched func #{name} on line #{line_no}"
+          yield :defs, name[0], line_no: line_no+1
         end
         line.scan(/^func \(\w+ \*?(\w+)\) (\w+)/) do |boundType, name|
-          p "matched func #{boundType}::#{name} on line #{line_no}"
+          yield :defs, name, line_no: line_no+1, scope: [boundType]
         end
         line.scan(/^package (\w+)/) do |name|
-          p "matched package #{name} on line #{line_no}"
+          yield :defs, name[0], line_no: line_no+1
         end
         line.scan(/^type (\w+) (\w+)/) do |newName, oldName|
-          p "matched type #{newName}  #{oldName} on line #{line_no}"
+          yield :defs, newName, line_no: line_no+1
         end
       end
     end
