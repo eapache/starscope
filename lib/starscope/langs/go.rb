f5f943cf463de7fc1655747d2725c3579951a147
Evan Huus <evan.huus@jadedpixel.com>
2013-08-08 15:12:04 -0400

    wip

--- lib/starscope/langs/go.rb
+++ lib/starscope/langs/go.rb
@@ -5,9 +5,19 @@ module StarScope::Lang
     end
 
     def self.extract(file)
-      str = File.read(file)
-      str.scan(/^func (\w+)/) do |match|
-        p match
+      str = File.readlines(file).each_with_index do |line, line_no|
+        line.scan(/^func (\w+)/) do |name|
+          p "matched func #{name} on line #{line_no}"
+        end
+        line.scan(/^func \(\w+ \*?(\w+)\) (\w+)/) do |boundType, name|
+          p "matched func #{boundType}::#{name} on line #{line_no}"
+        end
+        line.scan(/^package (\w+)/) do |name|
+          p "matched package #{name} on line #{line_no}"
+        end
+        line.scan(/^type (\w+) (\w+)/) do |newName, oldName|
+          p "matched type #{newName}  #{oldName} on line #{line_no}"
+        end
       end
     end
   end
