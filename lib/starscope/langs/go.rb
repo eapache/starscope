8c4f2b98a2bebd038a246e26762c5af21aa9d83f
Evan Huus <evan.huus@jadedpixel.com>
2013-08-09 16:08:18 -0400

    wip

--- lib/starscope/langs/go.rb
+++ lib/starscope/langs/go.rb
@@ -5,18 +5,47 @@ module StarScope::Lang
     end
 
     def self.extract(file)
+      stack = []
       str = File.readlines(file).each_with_index do |line, line_no|
-        line.scan(/^func (\w+)/) do |name|
-          yield :defs, name[0], line_no: line_no+1
+        # strip single-line comments like // foo
+        match = /(.*)\/\//.match(line)
+        if match
+          line = match[0]
         end
-        line.scan(/^func \(\w+ \*?(\w+)\) (\w+)/) do |boundType, name|
-          yield :defs, name, line_no: line_no+1, scope: [boundType]
-        end
-        line.scan(/^package (\w+)/) do |name|
-          yield :defs, name[0], line_no: line_no+1
-        end
-        line.scan(/^type (\w+) (\w+)/) do |newName, oldName|
-          yield :defs, newName, line_no: line_no+1
+        # poor-man's parser
+        case stack[-1]
+        when :comment
+          if /\*\// =~ line
+            stack.pop
+          end
+        when :struct
+          if /}/ =~ line
+            stack.pop
+          end
+        when :def
+          if /\)/ =~ line
+            stack.pop
+          end
+        when :import
+          if /\)/ =~ line
+            stack.pop
+          end
+        else
+          case line
+          when /^func\s+(\w+)\(/
+            yield :defs, $1, line_no: line_no+1
+          when /^package\s+(\w+)/
+            yield :defs, $1, line_no: line_no+1
+          when /^type\s+(\w+)\s+struct\s*{/
+            yield :defs, $1, line_no: line_no+1
+            stack.push(:struct)
+          when /^type\s+(\w+)/
+            yield :defs, $1, line_no: line_no+1
+          when /^import\s+(\w+)/
+            yield :imports, $1, line_no: line_no+1
+          when /^var/
+          when /^const/
+          end
         end
       end
     end
