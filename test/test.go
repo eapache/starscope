c12e8bbf0ea2b0d849422402ef24043234a1ac63
Evan Huus <eapache@gmail.com>
2014-01-10 17:40:28 -0500

    improve test, formatting'

--- test/test.go
+++ test/test.go
@@ -8,14 +8,18 @@ func b() int {
 	return 0
 }
 
-func c(a,b int) int {
+func c(a, b int) int {
 	return 1
 }
 
 func main() {
+	var (
+		q int
+		t string
+	)
 	x := a(1)
 	y := b()
-	z := c(a(b()), b())
+	z := c(a(q), b())
 
 	a(c(b(), b()))
 }
