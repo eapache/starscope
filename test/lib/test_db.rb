54a2213520823a38efa2883a887c59de94048de8
Evan Huus <eapache@gmail.com>
2014-01-11 17:08:34 -0500

    simple db update tests

--- test/lib/test_db.rb
+++ test/lib/test_db.rb
@@ -10,10 +10,28 @@ describe StarScope::DB do
     proc {@db.dump_table(:foo)}.must_raise StarScope::DB::NoTableError
   end
 
-  it "must accept adding new files and directories" do
-    @db.add_paths([GOLANG_SAMPLE, 'lib'])
+  it "must correctly add paths" do
+    paths = [GOLANG_SAMPLE, 'test/files']
+    @db.add_paths(paths)
+    @db.instance_eval('@paths').must_equal paths
     @db.instance_eval('@files').keys.must_include GOLANG_SAMPLE
-    @db.instance_eval('@files').keys.must_include 'lib/starscope/langs/ruby.rb'
+    @db.instance_eval('@files').keys.must_include RUBY_SAMPLE
+  end
+
+  it "must correctly pick up new files in old paths" do
+    @db.instance_eval('@paths = ["test/files"]')
+    @db.update
+    files = @db.instance_eval('@files').keys
+    files.must_include GOLANG_SAMPLE
+    files.must_include RUBY_SAMPLE
+  end
+
+  it "must correctly remove old files in existing paths" do
+    @db.instance_eval('@paths = ["test/files"]')
+    @db.instance_eval('@files = {"test/files/foo"=>"2012-01-01"}')
+    @db.instance_eval('@files').keys.must_include 'test/files/foo'
+    @db.update
+    @db.instance_eval('@files').keys.wont_include 'test/files/foo'
   end
 
 end
