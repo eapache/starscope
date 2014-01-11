da425fd18c30bd75c75de1f4b37ccdb7e7dcbd62
Evan Huus <eapache@gmail.com>
2014-01-11 17:18:29 -0500

    Add test for loading old db

--- test/lib/test_db.rb
+++ test/lib/test_db.rb
@@ -34,4 +34,11 @@ describe StarScope::DB do
     @db.instance_eval('@files').keys.wont_include 'test/files/foo'
   end
 
+  it "must correctly load an old DB file" do
+    @db.load('test/files/db_old.json.gz')
+    @db.instance_eval('@paths').must_equal ['test/files']
+    @db.instance_eval('@files').keys.must_include GOLANG_SAMPLE
+    @db.instance_eval('@files').keys.must_include RUBY_SAMPLE
+  end
+
 end
