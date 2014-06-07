d051bfefe3109e2549d043195a8d025128b211b0
Evan Huus <eapache@gmail.com>
2014-06-06 22:37:10 -0400

    Tidy golang parsing code

--- lib/starscope/langs/go.rb
+++ lib/starscope/langs/go.rb
@@ -18,51 +18,39 @@ module StarScope::Lang
       scope = []
       str = File.readlines(file).each_with_index do |line, line_no|
         line_no += 1 # zero-index to one-index
+
         # strip single-line comments like // foo
         match = /(.*)\/\//.match(line)
-        if match
-          line = match[1]
-        end
+        line = match[1] if match
         # strip single-line comments like foo /* foo */ foo
         match = /(.*?)\/\*.*\*\/(.*)/.match(line)
-        if match
-          line = match[1] + match[2]
-        end
+        line = match[1] + match[2] if match
         # strip end-of-line comment starters like foo /* foo \n
         match = /(.*?)\/\*/.match(line)
-        if match
-          line = match[1]
-          ends_with_comment = true
-        else
-          ends_with_comment = false
-        end
+        line = match[1] if match
+        ends_with_comment = !match.nil?
+
         # if we're in a block comment, wait for it to end
         if stack[-1] == :comment
           match = /\*\/(.*)/.match(line)
-          if match
-            line = match[1]
-            stack.pop
-          else
-            next
-          end
+          next unless match
+          line = match[1]
+          stack.pop
         end
+
         # poor-man's parser
         case stack[-1]
         when :struct
           case line
           when END_OF_BLOCK
-            yield :end, scope + ["}"], :line_no => line_no, :type => :class
-            stack.pop
-            scope.pop
+            end_block(line_no, scope, stack, &block)
           when /(.+)\s+\w+/
             parse_def($1, line_no, scope, &block)
           end
         when :interface
           case line
           when END_OF_BLOCK
-            yield :end, scope + ["}"], :line_no => line_no, :type => :class
-            stack.pop
-            scope.pop
+            end_block(line_no, scope, stack, &block)
           when /(\w+)\(.*\)\s+/
             yield :defs, scope + [$1], :line_no => line_no
           end
@@ -83,65 +71,75 @@ module StarScope::Lang
             name = $1.split('/')
             yield :imports, name, :line_no => line_no
           end
-        else
-          if stack[-1] == :func and /^\}/ =~ line
+        when :func
+          case line
+          when /^\}/
             yield :end, "}", :line_no => line_no, :type => :func
             stack.pop
-          end
-          case line
-          when /^func\s+(\w+)\(/
-            yield :defs, scope + [$1], :line_no => line_no, :type => :func
-            stack.push(:func)
-          when /^func\s+\(\w+\s+\*?(\w+)\)\s*(\w+)\(/
-            yield :defs, scope + [$1, $2], :line_no => line_no, :type => :func
-            stack.push(:func)
-          when /^package\s+(\w+)/
-            scope.push($1)
-            yield :defs, scope, :line_no => line_no, :type => :package
-          when /^type\s+(\w+)\s+struct\s*\{/
-            scope.push($1)
-            stack.push(:struct)
-            yield :defs, scope, :line_no => line_no, :type => :class
-          when /^type\s+(\w+)\s+interface\s*\{/
-            scope.push($1)
-            stack.push(:interface)
-            yield :defs, scope, :line_no => line_no, :type => :class
-          when /^type\s+(\w+)/
-            yield :defs, scope + [$1], :line_no => line_no, :type => :type
-          when /^import\s+"(.+)"/
-            name = $1.split('/')
-            yield :imports, name, :line_no => line_no
-          when /^import\s+\(/
-            stack.push(:import)
-          when /^var\s+\(/
-            stack.push(:def)
-          when /^var\s+(\w+)\s/
-            yield :defs, scope + [$1], :line_no => line_no
-          when /^const\s+\(/
-            stack.push(:def)
-          when /^const\s+(\w+)\s/
-            yield :defs, scope + [$1], :line_no => line_no
-          when /^\s+(.*?) :?=[^=]/
-            $1.split(' ').each do |var|
-              next if CONTROL_KEYS.include?(var)
-              name = var.delete(',').split('.')
-              next if name[0] == "_" # assigning to _ is a discard in golang
-              if name.length == 1
-                yield :assigns, scope + [name[0]], :line_no => line_no
-              else
-                yield :assigns, name, :line_no => line_no
-              end
-            end
-            parse_call(line, line_no, scope, &block)
           else
-            parse_call(line, line_no, scope, &block)
+            parse_new_line(line, line_no, scope, stack, &block)
           end
+        else
+          parse_new_line(line, line_no, scope, stack, &block)
         end
+
         # if the line looks like "foo /* foo" then we enter the comment state
         # after parsing the usable part of the line
         if ends_with_comment
           stack.push(:comment)
         end
+      end
+    end
+
+    # handles new lines (when not in the middle of an existing definition)
+    def self.parse_new_line(line, line_no, scope, stack, &block)
+      case line
+      when /^func\s+(\w+)\(/
+        yield :defs, scope + [$1], :line_no => line_no, :type => :func
+        stack.push(:func)
+      when /^func\s+\(\w+\s+\*?(\w+)\)\s*(\w+)\(/
+        yield :defs, scope + [$1, $2], :line_no => line_no, :type => :func
+        stack.push(:func)
+      when /^package\s+(\w+)/
+        scope.push($1)
+        yield :defs, scope, :line_no => line_no, :type => :package
+      when /^type\s+(\w+)\s+struct\s*\{/
+        scope.push($1)
+        stack.push(:struct)
+        yield :defs, scope, :line_no => line_no, :type => :class
+      when /^type\s+(\w+)\s+interface\s*\{/
+        scope.push($1)
+        stack.push(:interface)
+        yield :defs, scope, :line_no => line_no, :type => :class
+      when /^type\s+(\w+)/
+        yield :defs, scope + [$1], :line_no => line_no, :type => :type
+      when /^import\s+"(.+)"/
+        name = $1.split('/')
+        yield :imports, name, :line_no => line_no
+      when /^import\s+\(/
+        stack.push(:import)
+      when /^var\s+\(/
+        stack.push(:def)
+      when /^var\s+(\w+)\s/
+        yield :defs, scope + [$1], :line_no => line_no
+      when /^const\s+\(/
+        stack.push(:def)
+      when /^const\s+(\w+)\s/
+        yield :defs, scope + [$1], :line_no => line_no
+      when /^\s+(.*?) :?=[^=]/
+        $1.split(' ').each do |var|
+          next if CONTROL_KEYS.include?(var)
+          name = var.delete(',').split('.')
+          next if name[0] == "_" # assigning to _ is a discard in golang
+          if name.length == 1
+            yield :assigns, scope + [name[0]], :line_no => line_no
+          else
+            yield :assigns, name, :line_no => line_no
+          end
+        end
+        parse_call(line, line_no, scope, &block)
+      else
+        parse_call(line, line_no, scope, &block)
       end
     end
 
@@ -170,6 +168,12 @@ module StarScope::Lang
         yield :defs, scope + [var.delete(',')], :line_no => line_no
         break if not var.end_with?(',')
       end
+    end
+
+    def self.end_block(line_no, scope, stack)
+      yield :end, scope + ["}"], :line_no => line_no, :type => :class
+      stack.pop
+      scope.pop
     end
   end
 end
