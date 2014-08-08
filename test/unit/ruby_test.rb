7102b8ae07fd8ddec754c4d51df7fb800dd58d7d
Evan Huus <eapache@gmail.com>
2014-08-08 19:44:46 -0400

    Convert ruby tests to specs

--- test/unit/ruby_test.rb
+++ test/unit/ruby_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class TestRuby < Minitest::Test
-  def setup
+describe Starscope::Lang::Ruby do
+  before do
     @db = {}
     Starscope::Lang::Ruby.extract(RUBY_SAMPLE) do |tbl, name, args|
       @db[tbl] ||= []
@@ -9,52 +9,58 @@ class TestRuby < Minitest::Test
     end
   end
 
-  def test_recognition
-    assert Starscope::Lang::Ruby.match_file(RUBY_SAMPLE)
-    assert Starscope::Lang::Ruby.match_file('bin/starscope')
-    refute Starscope::Lang::Ruby.match_file(GOLANG_SAMPLE)
-    refute Starscope::Lang::Ruby.match_file(EMPTY_FILE)
+  it "must match ruby files" do
+    Starscope::Lang::Ruby.match_file(RUBY_SAMPLE).must_equal true
+    Starscope::Lang::Ruby.match_file('bin/starscope').must_equal true
   end
 
-  def test_function_defs
-    assert_includes @db.keys, :defs
+  it "must not match non-ruby files" do
+    Starscope::Lang::Ruby.match_file(GOLANG_SAMPLE).must_equal false
+    Starscope::Lang::Ruby.match_file(EMPTY_FILE).must_equal false
+  end
+
+  it "must identify function definitions" do
+    @db.keys.must_include :defs
     defs = @db[:defs].map {|x| x[:name][-1]}
-    assert_includes defs, :DB
-    assert_includes defs, :NoTableError
-    assert_includes defs, :load
-    assert_includes defs, :update
-    assert_includes defs, :files_from_path
+
+    defs.must_include :DB
+    defs.must_include :NoTableError
+    defs.must_include :load
+    defs.must_include :update
+    defs.must_include :files_from_path
   end
 
-  def test_constant_defs
-    assert_includes @db[:defs].map {|x| x[:name][-1]}, :PBAR_FORMAT
+  it "must identify constant definitions" do
+    @db[:defs].map {|x| x[:name][-1]}.must_include :PBAR_FORMAT
   end
 
-  def test_ends
-    assert_includes @db.keys, :end
-    assert_equal 13, @db[:end].count
+  it "must identify endings" do
+    @db.keys.must_include :end
+    @db[:end].count.must_equal 13
   end
 
-  def test_function_calls
-    assert_includes @db.keys, :calls
+  it "must identify function calls" do
+    @db.keys.must_include :calls
     calls = @db[:calls].group_by {|x| x[:name][-1]}
-    assert_includes calls.keys, :add_file
-    assert_includes calls.keys, :each
-    assert_equal 3, calls[:add_file].count
-    assert_equal 8, calls[:each].count
+
+    calls.keys.must_include :add_file
+    calls.keys.must_include :each
+    calls[:add_file].count.must_equal 3
+    calls[:each].count.must_equal 8
   end
 
-  def test_variable_assigns
-    assert_includes @db.keys, :assigns
+  it "must identify variable assignments" do
+    @db.keys.must_include :assigns
     assigns = @db[:assigns].group_by {|x| x[:name][-1]}
-    assert_includes assigns.keys, :pbar
-    assert_includes assigns.keys, :PBAR_FORMAT
-    assert_includes assigns.keys, :foo
-    assert_equal 2, assigns[:pbar].count
-    assert_equal 1, assigns[:PBAR_FORMAT].count
-    assert_equal 1, assigns[:foo].count
 
-    refute_includes assigns.keys, "=".to_sym
-    refute_includes assigns.keys, "<".to_sym
+    assigns.keys.must_include :pbar
+    assigns.keys.must_include :PBAR_FORMAT
+    assigns.keys.must_include :foo
+    assigns[:pbar].count.must_equal 2
+    assigns[:PBAR_FORMAT].count.must_equal 1
+    assigns[:foo].count.must_equal 1
+
+    assigns.keys.wont_include "=".to_sym
+    assigns.keys.wont_include "<".to_sym
   end
 end
